8e25590b07ee26746a75cdadfc6be15957bfa1f0
arifimran5 <arifimran505@gmail.com>
2023-01-07 00:24:48 +0530

    update bg gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Dropdown from './components/Dropdown';
 import Dialog from './components/Dialog';
 import RadioGroup from './components/RadioGroup';
+import CheckBox from './components/CheckBox';
 
 function App() {
   return (
-    <main className='min-h-screen bg-gradient-to-br from-pink-400 via-purple-500 to-indigo-500'>
+    <main className='min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-green-700 text-white'>
       <div className='pt-4 ml-4 '>
         <div className='wrapper'>
           <h1 className='heading'>1. Dropdown</h1>
@@ -17,6 +18,10 @@ function App() {
         <div className='wrapper'>
           <h1 className='heading'>3. Radio Group</h1>
           <RadioGroup />
+        </div>
+        <div className='wrapper'>
+          <h1 className='heading'>4. CheckBox</h1>
+          <CheckBox />
         </div>
       </div>
     </main>
